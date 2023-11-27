a96f5064d132b48a7ddb529aa38f126b01c93b3f
Sirio <sirioroberto1@hotmail.com>
2023-11-27 14:37:23 -0300

    Adjusted home icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
